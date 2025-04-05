8acaa0e44f0b9ce0d29c59e2e7d979168961dd56
Антон <frostik96@gmail.com>
2025-04-05 08:58:15 +0300

    feat:готово к работе

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import config
+
+async def main():
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher(bot=bot)
+    # dp.include_router()
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
+
