b3ba66f02843e084605e8da79ec956b45885bf47
Антон <frostik96@gmail.com>
2025-04-09 13:27:34 +0300

    feat:метод serilize_sheet_token_file

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
+from pprint import pprint
+import json
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
+    SHEETS_TOKEN_FILE: str
+    SHEET_NAME: str
 
     model_config = SettingsConfigDict(env_file='.env')
+
+    def serialize_sheet_token_file(self):
+        serialize = json.loads(self.SHEETS_TOKEN_FILE)
+        return serialize
 
 
 config = Settings()
