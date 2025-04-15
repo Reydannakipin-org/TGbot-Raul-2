f7f69162e300134d49ea5d318a4287d97e73f2d0
Антон <frostik96@gmail.com>
2025-04-15 04:19:09 +0300

    feat: два новых класса клавиатуры

--- keyboards/reply_kb.py
+++ keyboards/reply_kb.py
@@ -1,6 +1,9 @@
 from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from keyboards.constans import BUTTON_USER, BUTTON_ADMIN, BUTTON_MENU
+from utils.lexicon import menu_buttons, YES_NO
+
+
 class ReplyBuilder:
     def __init__(self, buttons, rows=1):
         self.keyboard = ReplyKeyboardBuilder()
@@ -20,3 +23,16 @@ class UserKeyboard(ReplyBuilder):
 class AdminKeyboard(ReplyBuilder):
     def __init__(self):
         super().__init__(BUTTON_ADMIN.keys(), rows=2)
+
+
+class AntonKeyboard(ReplyBuilder):
+    """Инициализирует клавиатуру с кнопками в зависимости от роли."""
+
+    def __init__(self, role):
+        super().__init__(menu_buttons[role], rows=2)
+
+class YesNoKeyboard(ReplyBuilder):
+    """Инициализирует клавиатуру с кнопками Да/Нет."""
+
+    def __init__(self):
+        super().__init__(YES_NO.keys(), rows=2)
