f6cb2b8f8611cdba05ba7141ea7e70d6e92e927c
Антон <frostik96@gmail.com>
2025-04-15 04:14:56 +0300

    chore:Закомментил класс

--- config.py
+++ config.py
@@ -3,13 +3,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Настройки для переменных окружения"""
+
     BOT_TOKEN: str
     SHEETS_TOKEN_FILE: str
     SHEET_ID: str
+    CHAT_ID: int
 
-    model_config = SettingsConfigDict(env_file='.env', env_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     def serialize_sheet_token_file(self):
+        """Метод для преобразования файла с токеном для доступа к таблице"""
         serialize = json.loads(self.SHEETS_TOKEN_FILE)
         return serialize
 
