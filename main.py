c4990ac5b4a9a1b3e8fe34717696a98fa3fb6305
Антон Серебряков <antonserebrakov@MacBook-Air-Anton.local>
2025-04-24 16:19:02 +0200

    First version of Draw

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
+from daemons.drawdaemon import daemon_loop
 from config import config
 from handlers.main_handlers import KeyboardHandler
 
 
-async def main():
-    """Главная функция запуска бота"""
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+async def start_bot():
     bot = Bot(token=config.BOT_TOKEN)
-    dp = Dispatcher(bot=bot)
+    dp = Dispatcher()
     dp.include_router(KeyboardHandler())
+    logger.info('Запуск Telegram-бота...')
     await dp.start_polling(bot)
+
+
+async def main():
+    logger.info('Запуск основного приложения...')
+    await asyncio.gather(
+        daemon_loop(),
+        start_bot()
+    )
+
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-
